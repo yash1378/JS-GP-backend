162caa991d459482bd9286f6ee9883231ae6b5b2
yash1378 <slayycasm997@gmail.com>
2024-06-01 15:16:56 +0530

    razorpay api chekcing

--- controllers/Razorpay.go
+++ controllers/Razorpay.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/razorpay/razorpay-go"
 	"github.com/razorpay/razorpay-go/utils"
 )
 
@@ -19,30 +18,46 @@ type veri struct {
 	Sign    string `json:"razorpay_signature"`
 }
 
+// func Order(c *gin.Context) {
+// 	var input order
+// 	fmt.Println(c.Request.Body)
+// 	if err := c.ShouldBindBodyWithJSON(&input); err != nil {
+// 		fmt.Println(input)
+// 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	// println(input)
+// 	// amt, err := strconv.Atoi(input.Amount)
+// 	client := razorpay.NewClient("rzp_test_gAnipV9pamMXV2", "cUhcdokPA3QwiULcMmadGx86")
+
+// 	data := map[string]interface{}{
+// 		"amount":   input.Amount * 100, // amount in paise, so 50000 paise = INR 500
+// 		"currency": "INR",
+// 		"receipt":  "some_receipt_id",
+// 	}
+// 	body, err := client.Order.Create(data, nil)
+// 	if err != nil {
+// 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	c.JSON(http.StatusOK, gin.H{"message": body})
+// }
+
 func Order(c *gin.Context) {
-	var input order
-	fmt.Println(c.Request.Body)
-	if err := c.ShouldBindBodyWithJSON(&input); err != nil {
+	var input map[string]interface{}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
 		fmt.Println(input)
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	// println(input)
-	// amt, err := strconv.Atoi(input.Amount)
-	client := razorpay.NewClient("rzp_test_gAnipV9pamMXV2", "cUhcdokPA3QwiULcMmadGx86")
 
-	data := map[string]interface{}{
-		"amount":   input.Amount * 100, // amount in paise, so 50000 paise = INR 500
-		"currency": "INR",
-		"receipt":  "some_receipt_id",
-	}
-	body, err := client.Order.Create(data, nil)
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+	// Print the input received from the client
+	fmt.Println(input)
 
-	c.JSON(http.StatusOK, gin.H{"message": body})
+	// Return the input in the response
+	c.JSON(http.StatusOK, gin.H{"message": input})
 }
 
 func Key(c *gin.Context) {
